70e2d2aec425154c40235c1a63096f987425287c
Giovanni Leon <giovannileonp@gmail.com>
2021-06-23 15:50:49 -0500

    add model and migration2

--- Platzigram/posts/models.py
+++ Platzigram/posts/models.py
@@ -8,7 +8,7 @@ class User(models.Model):
     password = models.CharField(max_length = 100)
     first_name = models.CharField(max_length = 100)
     last_name = models.CharField(max_length = 100)
-
+    is_admin = models.BooleanField(default=False)
     bio = models.TextField(blank=True)
     birthdate = models.DateField(blank = True, null = True)
     created = models.DateTimeField(auto_now_add = True)
