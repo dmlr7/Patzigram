47c082011f8cdbd1c4581157c542f0425a2d3996
Giovanni Leon <giovannileonp@gmail.com>
2021-06-24 17:47:40 -0500

    add model for posts and the corresponding migration

--- Platzigram/posts/models.py
+++ Platzigram/posts/models.py
@@ -1,4 +1,24 @@
 #django
 from django.db import models
+from django.contrib.auth.models import User
 #utils
 
+
+class Post(models.Model):
+    """Post Model
+    """
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    profile = models.ForeignKey('users.Profile', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    photo = models.ImageField(upload_to='posts/photos')
+
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        """Return title and username
+
+        Returns:
+            str: title and username
+        """
+        return f'{self.title} by @{self.user.username}'
