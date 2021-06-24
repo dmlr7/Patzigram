4b4fd811f26aefc0059c7c273ce947094a3f8dcb
Giovanni Leon <giovannileonp@gmail.com>
2021-06-24 15:29:37 -0500

    modify the admin foir profile model, minor fix in the profile model

--- Platzigram/users/admin.py
+++ Platzigram/users/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import Profile
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    """Profile admin"""
+    list_display = ('pk', 'user', 'phone_number', 'website', 'picture')
+    list_display_links = ('pk', 'user')
+    list_editable = ('phone_number', 'website', 'picture')
+    search_fields = (
+        'user__email', 
+        'user__first_name',
+        'user__last_name',
+        'phone_number',
+        'user__username',
+        )
+    list_filter = (
+        'created',
+        'modified',
+        'user__is_active',
+        'user__is_staff',
+        )
--- Platzigram/users/models.py
+++ Platzigram/users/models.py
@@ -8,7 +8,7 @@ class Profile(models.Model):
     
     website = models.URLField(max_length=200, blank=True)
     biography = TextField(blank=True)
-    phone_number = models.CharField(max_length=20)
+    phone_number = models.CharField(max_length=20, blank=True)
     picture = models.ImageField(upload_to='users/pictures', 
         blank=True, null= True)
     created = models.DateTimeField(auto_now_add=True)
