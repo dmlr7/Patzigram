83f122b99431d134cb8aad76afce1e7e85f89822
Giovanni Leon <giovannileonp@gmail.com>
2021-06-24 18:58:20 -0500

    modify the media routing

--- Platzigram/Platzigram/urls.py
+++ Platzigram/Platzigram/urls.py
@@ -13,8 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+
+
 from Platzigram import views as local_views
 from posts import views as post_views
 
@@ -25,4 +30,6 @@ urlpatterns = [
     path('hi/<str:name>/<int:age>/', local_views.say_hi),
     path('posts/', post_views.list_posts),
     
-]
+] + static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT)
--- Platzigram/posts/models.py
+++ Platzigram/posts/models.py
@@ -1,3 +1,4 @@
+    """Posts Models"""
 #django
 from django.db import models
 from django.contrib.auth.models import User
