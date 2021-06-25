49851f7df8494011651e93514941361bbcb2f1ab
Giovanni Leon <giovannileonp@gmail.com>
2021-06-25 09:31:43 -0500

    add controler for signup process

--- Platzigram/users/views.py
+++ Platzigram/users/views.py
@@ -1,7 +1,16 @@
 """Users views."""
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import redirect, render
+
+# Django
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+
+# Exception
+from django.db.utils import IntegrityError
+
+# Models
+from django.contrib.auth.models import User
+from users.models import Profile
 
 def login_view(request):
     """Login view.
@@ -23,9 +32,41 @@ def login_view(request):
             return render(request, 'users/login.html', {'error': 'Invalid username or password'})
     return render(request,'users/login.html')
     
+
+
+
+def signup_view(request):
+    """Sign up view."""
+    if request.method == 'POST':
+        username = request.POST['username']
+        passwd = request.POST['passwd']
+        passwd_confirmation = request.POST['passwd_confirmation']
+
+        if passwd != passwd_confirmation:
+            return render(request, 'users/signup.html', {'error': 'Password confirmation does not match'})
+
+        try:
+            user = User.objects.create_user(username=username, password=passwd)
+        except IntegrityError:
+            return render(request, 'users/signup.html', {'error': 'Username is already in user'})
+
+        user.first_name = request.POST['first_name']
+        user.last_name = request.POST['last_name']
+        user.email = request.POST['email']
+        user.save()
+
+        profile = Profile(user=user)
+        profile.save()
+
+        return redirect('login')
+
+    return render(request, 'users/signup.html')
+
+
+
 @login_required
 def logout_view(request):
-    """logout user.
+    """Logout user.
 
     Args:
         request
@@ -35,7 +76,3 @@ def logout_view(request):
     """
     logout(request)
     return redirect('login')
-
-def signup_view(request):
-    """Dignup view."""
-    return render(request,'users/signup.html')
