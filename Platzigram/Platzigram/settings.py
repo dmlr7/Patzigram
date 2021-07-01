dcbffac581fff98393ad8a4a2eb9ed6d8b675ef8
Giovanni Leon <giovannileonp@gmail.com>
2021-07-01 15:16:22 -0500

    add generic templates for login and logout actions, fixed urls calls, configured static variables in settings for redirect and logout

--- Platzigram/Platzigram/settings.py
+++ Platzigram/Platzigram/settings.py
@@ -138,4 +138,7 @@ MEDIA_URL = "/media/"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_URL = '/users/login'
+
+LOGIN_URL = '/users/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = LOGIN_URL
--- Platzigram/users/views.py
+++ Platzigram/users/views.py
@@ -4,6 +4,7 @@
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import views as auth_views
 from django.shortcuts import render, redirect
 from django.urls import reverse, reverse_lazy
 from django.views.generic import DetailView, FormView, UpdateView
@@ -65,39 +66,13 @@ class UpdateProfileView(LoginRequiredMixin, UpdateView):
 
 
 
-def login_view(request):
-    """Login view.
+class LoginView(auth_views.LoginView):
+    """Login view."""
 
-    Args:
-        request (request): request data
-    Returns:
-        HttpResponse: render users/login.hrml
-    """
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        #print(username,password)
-        user = authenticate(request,username=username,password=password)
-        if user:
-            login(request,user)
-            return redirect('posts:feed')
-        else:
-            return render(request, 'users/login.html', {'error': 'Invalid username or password'})
-    return render(request,'users/login.html')
-    
+    template_name = 'users/login.html'
 
 
+class LogoutView(LoginRequiredMixin, auth_views.LogoutView):
+    """Logout view."""
 
-
-@login_required
-def logout_view(request):
-    """Logout user.
-
-    Args:
-        request
-
-    Returns:
-        redirect to login
-    """
-    logout(request)
-    return redirect('users:login')
+    template_name = 'users/logged_out.html'
